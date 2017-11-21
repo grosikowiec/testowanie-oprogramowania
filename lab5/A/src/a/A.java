9649b5ff6132f066c6bfc384c3749bfef260fdf2
grosikowiec <32505393+grosikowiec@users.noreply.github.com>
2017-11-21 11:22:21 +0100

    Add files via upload

---
+++ lab5/A/src/a/A.java
@@ -0,0 +1,17 @@
+
+package a;
+
+
+public class A {
+
+    public  int a(int x) {
+        
+        if(x>0)            
+            return 1;
+        else if(x==0)
+            return 0;
+        else
+            return -1;   
+    }
+    
+}
---
+++ lab5/A/src/a/TestA.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+/**
+ *
+ * @author Student
+ */
+public class TestA {
+    @Test
+    public void test a1(){
+    A a = new A();
+    AssertTrue(a,a.a(1));
+    AssertFalse();
+    AssertNotNull();
+    AssertNull();
+    AssertEqual(1,a.a(1));
+    }
+    
+    @BeforeTest
+    
+    @AfterTest
+    
+    @Before
+    
+    @After
+    
+}
